44030bf007f843432f858aa38a74e90ae10ce7ee
Anselm Schaefer <schaefab@basfad.basf.net>
2022-10-12 22:56:13 +0200

    add proper REST service and microservice connection

---
+++ src/main/java/com/anselm/ratingsdataservice/models/UserRating.java
@@ -0,0 +1,14 @@
+package com.anselm.ratingsdataservice.models;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRating {
+	
+	private List<Rating> userRating;
+
+}
